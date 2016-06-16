a1c2fdc09c0e49917f8d3c055ad869df5eba4ca3
Bertrand Martel <bmartel.fr@gmail.com>
2016-06-17 01:34:03 +0200

    remove hard coded clientId

--- views/oauth/index.js
+++ views/oauth/index.js
@@ -2,6 +2,7 @@
 
 exports.init = function (req, res) {
     res.render('oauth/index', {
-        authentication_uri: req.app.config.authentication_thirdpart.webservice_uri
+        authentication_uri: req.app.config.authentication_thirdpart.webservice_uri,
+        client_id: req.app.config.google.clientId
     });
 };
