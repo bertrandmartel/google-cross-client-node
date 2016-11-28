256df96400628abfd78a68ff383291d422d81a76
Bertrand Martel <bmartel.fr@gmail.com>
2016-11-28 19:39:26 +0100

    id_token, access_token,client_check apis (client oauth)

--- app/api/external/index.js
+++ app/api/external/index.js
@@ -9,13 +9,13 @@ function sendError(code, message, res) {
     });
 }
 
-exports.authenticated = function(req, res) {
+exports.authenticated = function (req, res) {
 
     if ("deviceId" in req.body && req.body.deviceId != "") {
 
         var query = req.app.db.models.Device.find({}).select('is_device_login is_webservice_login -_id');
 
-        query.where('_id', req.body.deviceId).exec(function(err, value) {
+        query.where('_id', req.body.deviceId).exec(function (err, value) {
 
             if (err) {
                 sendError(500, "check database failure", res);
@@ -38,14 +38,66 @@ exports.authenticated = function(req, res) {
     }
 }
 
+exports.client_check = function (req, res) {
 
-exports.accesstoken = function(req, res) {
+    var authorization = req.headers['authorization'].split(/[ ]+/);
+
+    console.log("in client_check : " + authorization[2].trim());
+
+    if (authorization[2].trim() != "") {
+
+        req.app.reqmod('https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=' + authorization[2].trim(), function (error, response, body) {
+
+            if (error) {
+                console.log("request error");
+                sendError(500, "verification request error", res);
+            } else {
+                try {
+                    if (response.statusCode == 200) {
+
+                        var query = req.app.db.models.Device.find({}).select('_id');
+
+                        query.where('email', JSON.parse(response.body).email).exec(function (err, value) {
+
+                            if (err) {
+                                sendError(500, "check database failure", res);
+                            } else {
+                                console.log("in result");
+                                console.log(value);
+                                if (value.length > 0) {
+                                    console.log("success");
+                                    res.status(200).send({
+                                        "deviceId": value[0]._id
+                                    });
+                                } else {
+                                    console.log("verification failed");
+                                    sendError(500, "access token not found", res);
+                                }
+                            }
+                        });
+
+                    } else {
+                        sendError(500, "bad token", res);
+                    }
+                } catch (e) {
+                    sendError(500, "request format error", res);
+                }
+            }
+        });
+
+    } else {
+        sendError(500, "accessToken field is missing", res);
+    }
+}
+
+
+exports.accesstoken = function (req, res) {
 
     if ("accessToken" in req.body && req.body.accessToken != "") {
 
         var query = req.app.db.models.Device.find({}).select('_id');
 
-        query.where('access_token', req.body.accessToken).exec(function(err, value) {
+        query.where('access_token', req.body.accessToken).exec(function (err, value) {
 
             if (err) {
                 sendError(500, "check database failure", res);
@@ -58,6 +110,50 @@ exports.accesstoken = function(req, res) {
                     });
                 } else {
                     sendError(500, "access token not found", res);
+                }
+            }
+        });
+
+    } else {
+        sendError(500, "accessToken field is missing", res);
+    }
+}
+
+exports.id_token = function (req, res) {
+
+    if ("deviceId" in req.body && req.body.deviceId != "" && "id_token" in req.body && req.body.id_token != "") {
+
+        req.app.reqmod('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + req.body.id_token, function (error, response, body) {
+
+            if (error) {
+                sendError(500, "verification request error", res);
+            } else {
+                console.log(req.body.id_token);
+                try {
+                    if (response.statusCode == 200) {
+
+                        var query = req.app.db.models.Device.find({}).select('_id');
+
+                        query.where('_id', req.body.deviceId).exec(function (err, value) {
+
+                            if (err) {
+                                sendError(500, "check database failure", res);
+                            } else {
+                                console.log(value + " & " + body.email);
+                                if (value.length > 0 && body.email == value[0].email) {
+                                    console.log("success");
+                                    res.status(200).send({});
+                                } else {
+                                    sendError(500, "access token not found", res);
+                                }
+                            }
+                        });
+
+                    } else {
+                        sendError(500, "bad token", res);
+                    }
+                } catch (e) {
+                    sendError(500, "request format error", res);
                 }
             }
         });
