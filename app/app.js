a2993bec1298dabbb30d1b44141ea229a69358b6
Bertrand Martel <bmartel.fr@gmail.com>
2016-11-28 19:37:36 +0100

    fix jwt middleware for external apis

--- app/app.js
+++ app/app.js
@@ -212,7 +212,7 @@ function createApiRouter(config, morgan, accessLogStream) {
     router.app = app;
 
     if ("jwt" in app.config && "secret" in app.config.jwt && "private_key" in app.config.jwt) {
-        router.all(app.config.baseUrl + '/oauth/ext/*', [require('./middlewares/jwtcors')]);
+        router.all('/oauth/ext/*', [require('./middlewares/jwtcors')]);
     } else {
         console.log("jwt not specified in configuration");
     }
